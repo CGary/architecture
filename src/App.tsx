ee54300436b67aa68e372b2e3c74ff7e403c0216
Renzo Anaya <renzoansa@gmail.com>
2023-05-23 10:25:22 -0500

    currency converter acoplado

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
 import { UserId } from "./UserId";
+
 function App() {
+  const [exchangeRate, setExchangeRate] = useState();
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    const fetchExchangeRate = async () => {
+      const myHeaders = new Headers();
+      myHeaders.append("apikey", "uXM4imxrpkOXLiZAC3SrvPFbPcfsPWiC");
+      const response = await fetch(
+        "https://api.apilayer.com/exchangerates_data/latest?symbols=BOB&base=USD",
+        { method: "GET", headers: myHeaders }
+      );
+      const {
+        rates: { BOB: exchangeRate },
+      } = await response.json();
+
+      setExchangeRate(exchangeRate);
+    };
+
+    fetchExchangeRate();
+  }, []);
+
   return (
     <>
       <h1>Aplicación x</h1>
@@ -7,6 +30,15 @@ function App() {
       <div>
         <p>otros componentes</p>
       </div>
+      <input
+        value={value}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+      />
+      <span> dolares</span>
+      <p>son</p>
+      <p>{parseFloat(value) * (exchangeRate || 0)}</p>
     </>
   );
 }
